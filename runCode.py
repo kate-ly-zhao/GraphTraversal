6e3b4d74853c86556bbe52cde121b053cbfa57a5
Syed Haider Abidi <syed.haider.abidi@cern.ch>
2017-03-29 22:51:19 -0400

    testing scenario + total BW loading + code clean up

--- runCode.py
+++ runCode.py
@@ -14,6 +14,7 @@ def main():
     ######################################
     # define the site configuration
     ######################################
+
     option = OrderedDict()
     name = "HRPS"
     option["Name"]      = name
@@ -184,17 +185,29 @@ def main():
 
 def runTest(siteConfig, siteGraph):
     "This function is designed to run the bandwidth loading tests"
-    "Tests are 'hard' coded in place"
 
-    #############################
-    # Get all the loading senarios for Site to Prime test
-   
+    ####################
+    # MetaInformation
+    ####################
+
     # First list a list of unique 'Site' numbers that are avaliable in the network
     uniqueSite = []
     for key in siteConfig:
         uniqueSite.append(siteConfig[key]["Site"])
         pass
     uniqueSite = list(set(uniqueSite))
+
+    # first find the unique zones in the graph
+    uniqueZone = []
+    for key in siteConfig:
+        uniqueZone.append(siteConfig[key]["Zone"])
+        pass
+    uniqueZone = list(set(uniqueZone))
+
+
+   
+    print '---------------------------------------------------------'
+    print 'SUT to PUT loading\n'
 
     # for each site number, find all the Prime and GeoPrime sites
     uniquePrimeSite = {}
@@ -215,7 +228,7 @@ def runTest(siteConfig, siteGraph):
     # this is due to fact that 'Dispatch' locations have a site number designation of 0
     keysToDelete = []
     for key in uniquePrimeSite:
-        print key, uniquePrimeSite[key], len(uniquePrimeSite[key])
+        #print key, uniquePrimeSite[key], len(uniquePrimeSite[key])
         if(len(uniquePrimeSite[key]) == 0):
             keysToDelete.append(key)
             pass
@@ -224,7 +237,7 @@ def runTest(siteConfig, siteGraph):
         del uniquePrimeSite[key]
 
     # Debug Output
-    print uniquePrimeSite
+    print "Unique prime sites", uniquePrimeSite
 
     # for each Prime/GeoPrime site, find all the loading senarios for SUT to PUT graphs
     # Store this information for later use
@@ -232,38 +245,36 @@ def runTest(siteConfig, siteGraph):
     for siteNum in uniquePrimeSite:
         if(len(uniquePrimeSite[siteNum]) == 0):
             continue
-        SUT_to_PUT_loading[siteNum] = {}
 
         for site in uniquePrimeSite[siteNum]:
             print "Testing SUT to PUT loading", siteNum, site
-            SUT_to_PUT_loading[siteNum][site] = runSubSiteToPrimeTest(site, siteConfig, siteGraph)
+            SUT_to_PUT_loading[site] = runSubSiteToPrimeTest(site, siteConfig, siteGraph)
             pass
         pass
 
+    print '---------------------------------------------------------'
 
     ##################################
     # This is PUT to CUT loading senairo
     # Basic idea: create all combinations of Prime/GeoPrime at can be active for all site numbers
     # Similarly, create all combinations of core/DSR that can be active in a given zone
     # then compute the loading for all senarios
-
     # this is creating the correct input format for itertool to create the 
     # cartesian product of prime/GeoPrime sites
+
+    print '---------------------------------------------------------'
+    print 'PUT to CUT loading\n'
+
     primeSiteList = []
     for siteNum in uniquePrimeSite:
         primeSiteList.append(uniquePrimeSite[siteNum])
         pass
 
-    # Debug output
-    print primeSiteList
-    activePrimeSiteCombinations = itertools.product(*primeSiteList)
-
-    # first find the unique zones in the graph
-    uniqueZone = []
-    for key in siteConfig:
-        uniqueZone.append(siteConfig[key]["Zone"])
-        pass
-    uniqueZone = list(set(uniqueZone))
+    ## Debug output
+    print "Prime Site list: ", primeSiteList
+    #activePrimeSiteCombinations = itertools.product(*primeSiteList)
+    #for currActivePrime in itertools.product(*primeSiteList):
+    #    print "Prime Iter Site list: ", currActivePrime
 
     # for each zone, find the list of Cores/DSR cores
     uniqueZoneSite = {}
@@ -281,7 +292,7 @@ def runTest(siteConfig, siteGraph):
         pass
 
     # debug output
-    print uniqueZoneSite
+    print "Active core/DSR Core", uniqueZoneSite
 
     # For each zone in the graph
     # loop over all Core/DSRCore
@@ -291,34 +302,185 @@ def runTest(siteConfig, siteGraph):
         for activeCore in uniqueZoneSite[zoneNum]:
             PUT_TO_CUT_loading[activeCore] = {}
             for currActivePrime in itertools.product(*primeSiteList):
-                #print 'Running Loading', activeCore, currActivePrime
+                print 'Running PUT to CUT Loading', activeCore, currActivePrime
                 PUT_TO_CUT_loading[activeCore][currActivePrime] = runPrimeToCoreTest(activeCore, currActivePrime, siteConfig, siteGraph)
                 pass
             pass
         pass
+    print '---------------------------------------------------------'
 
 
     ##################################
     # This is DUT to CUT loading senairo
     # For each core, compute the loading senario for where all dispatch sites talk to an active core/DSR
     DUT_TO_CUT_loading = {}
-    
+    print '---------------------------------------------------------'
+    print 'DUT to CUT loading\n'
+
     for zoneNum in uniqueZoneSite:
         for activeCore in uniqueZoneSite[zoneNum]:
-            print "Testing active Core",activeCore
+            print "Testing active Core:",activeCore
             DUT_TO_CUT_loading[activeCore] = runDispatchToCoreTest(activeCore, siteConfig, siteGraph)
             pass
         pass
+    print '---------------------------------------------------------'
 
     ######################################
-    # TODO: Compute the GeoPrime -> Prime loading
-    # TODO: Compute the DSR -> Core Loading
-    # TODO: Sum all possible senarios and find the worst case loading for each node
+    # Compute the GeoPrime -> Prime loading
+
+    print '---------------------------------------------------------'
+    print 'GeoPrime to Prime loading\n'
 
 
+    # for each site number, find all the Prime and GeoPrime sites
+    primeToGeoPrimeList = {}
+    for siteNum in uniqueSite:
+        primeKey = None
+        geoPrimeKey = None
+        for key in siteConfig:
+            if('Prime' in siteConfig[key]['Type'] and siteConfig[key]['Site'] == siteNum):
+                primeKey = key
+                pass
+            if('GeoPrime' in siteConfig[key]['Type'] and siteConfig[key]['Site'] == siteNum):
+                geoPrimeKey = key
+                pass
+            pass
+        if(primeKey is not None and geoPrimeKey is not None):
+            primeToGeoPrimeList[primeKey] = geoPrimeKey
+            pass
+        pass
+
+    print primeToGeoPrimeList
+
+    PUT_TO_GPUT_loading = {}
+
+    for site in primeToGeoPrimeList:
+        print "Testing active prime:", site, 'GeoPrime:' ,primeToGeoPrimeList[site]
+        PUT_TO_GPUT_loading[site] = runPrimeToGeoPrimeTest(site, primeToGeoPrimeList[site], siteConfig, siteGraph)
+        # save a copy going the other way        
+        PUT_TO_GPUT_loading[primeToGeoPrimeList[site]] = runPrimeToGeoPrimeTest(site, primeToGeoPrimeList[site], siteConfig, siteGraph)
+        pass
+    print '---------------------------------------------------------'
+
+
+    # Compute the DSR -> Core Loading
+    print '---------------------------------------------------------'
+    print 'Core to DSR Core loading\n'
+
+
+    # for each site number, find all the Prime and GeoPrime sites
+    coreToDSRCoreList = {}
+    for zoneNum in uniqueZone:
+        coreKey = None
+        DSRCoreKey = None
+        for key in siteConfig:
+            if('Core' in siteConfig[key]['Type'] and siteConfig[key]['Zone'] == zoneNum):
+                coreKey = key
+                pass
+            if('DSRCore' in siteConfig[key]['Type'] and siteConfig[key]['Zone'] == zoneNum):
+                DSRCoreKey = key
+                pass
+            pass
+        if(coreKey is not None and DSRCoreKey is not None):
+            coreToDSRCoreList[coreKey] = DSRCoreKey
+            pass
+        pass
+
+    CUT_TO_DCUT_loading = {}
+
+    for site in coreToDSRCoreList:
+        print "Testing active Core:", site, 'DSRCore:' ,coreToDSRCoreList[site]
+        CUT_TO_DCUT_loading[site] = runCoreToDSRCoreTest(site, coreToDSRCoreList[site], siteConfig, siteGraph)
+        # save a copy going the other way
+        CUT_TO_DCUT_loading[coreToDSRCoreList[site]] = runCoreToDSRCoreTest(site, coreToDSRCoreList[site], siteConfig, siteGraph)
+        pass
+    print '---------------------------------------------------------'
+    
+    # Sum all possible scenarios and find the worst case loading for each node
+    # loop over each zone
+    for zoneNum in uniqueZone:
+        print "Curr Zone", zoneNum
+        # pick an active core or DSR core
+        for activeCore in uniqueZoneSite[zoneNum]:
+            print "Active Core:", activeCore
+            # pick an active prime/geoprime for each site
+            for currActivePrimes in itertools.product(*primeSiteList):
+                print "Active PrimeList:", currActivePrimes
+
+
+                # get the datastructure of bw loading
+                TotalBwLoad = getBwMap(siteConfig)
+
+                # add Core - DSR Core loading
+                if activeCore in CUT_TO_DCUT_loading:
+                    TotalBwLoad = addBwLoad(TotalBwLoad, CUT_TO_DCUT_loading[activeCore], siteConfig)
+                    pass
+
+                # add GeoPrime - Prime loading
+                for activePrime in currActivePrimes:
+                    if activePrime in PUT_TO_GPUT_loading:
+                        TotalBwLoad = addBwLoad(TotalBwLoad, PUT_TO_GPUT_loading[activePrime], siteConfig)
+                        pass
+
+                # DUT to active core
+                TotalBwLoad = addBwLoad(TotalBwLoad, DUT_TO_CUT_loading[activeCore], siteConfig)
+
+                # SUT to active prime
+                for activePrime in currActivePrimes:
+                    TotalBwLoad = addBwLoad(TotalBwLoad, SUT_to_PUT_loading[activePrime], siteConfig)
+
+                # add active prime to active core
+                TotalBwLoad = addBwLoad(TotalBwLoad, PUT_TO_CUT_loading[activeCore][currActivePrimes], siteConfig)
+
+
+                TotalBwLoad = cleanUpBWMatrix(TotalBwLoad, siteConfig)
+
+                # print for debugging
+                for key in siteConfig:
+                    for keyprime in siteConfig:
+                        if(TotalBwLoad[key][keyprime] == 0):
+                            continue
+                        print key, keyprime, TotalBwLoad[key][keyprime]
+                        pass
+                    pass
+                print '********************'
+                            
+                pass
+            pass
+        pass
+        
+
+    # TODO: 1 point failure scenarios
+    # TODO: Find max loading for each path, and tell which combination of active core and prime lead to that
+    # TODO: Allow for a physical site to be in multiple zones
     ########################################
     # Long term future list
     # GUI? :P
+
+
+def runCoreToDSRCoreTest(activeCore, activeDSR, siteConfig, siteGraph):
+    "This function computes the loading from active core site in a zone to active DSRCore location"
+
+    # get the datastructure of bw loading
+    bwLoad = getBwMap(siteConfig)
+
+    # load the network for all dispatch to active core
+    bwLoad = computeLoading([activeCore], activeDSR, bwLoad, 'DSRCore', siteConfig, siteGraph)
+    
+    return bwLoad
+
+
+def runPrimeToGeoPrimeTest(activePrime, activeGeoPrime, siteConfig, siteGraph):
+    "This function computes the loading from active prime site in a site to activeGeoPrime location"
+
+    # get the datastructure of bw loading
+    bwLoad = getBwMap(siteConfig)
+
+    # load the network for all dispatch to active core
+    bwLoad = computeLoading([activePrime], activeGeoPrime, bwLoad, 'GeoPrime', siteConfig, siteGraph)
+    
+    return bwLoad
+
 
 
 def runDispatchToCoreTest(activeCore, siteConfig, siteGraph):
@@ -411,13 +573,13 @@ def computeLoading(fromSiteList, toSite, bwLoad, BandwidthOption, siteConfig, si
         pass
 
     # print for debugging
-    for key in siteConfig:
-        for keyprime in siteConfig:
-            if(bwLoad[key][keyprime] == 0):
-                continue
-            print key, keyprime, bwLoad[key][keyprime]
-            pass
-        pass
+    #for key in siteConfig:
+    #    for keyprime in siteConfig:
+    #        if(bwLoad[key][keyprime] == 0):
+    #            continue
+    #        print key, keyprime, bwLoad[key][keyprime]
+    #        pass
+    #    pass
     return bwLoad
 
 
@@ -430,6 +592,57 @@ def getBwMap(siteConfig):
             bwLoad[key][keyprime] = 0
             pass
         pass
+    return bwLoad
+
+
+def addBwLoad(BW1, BW2, siteConfig):
+    "Add two BW loading scenario"    
+    # get the datastructure of bw loading
+    bwLoad = getBwMap(siteConfig)
+    for key in bwLoad:
+        for keyprime in bwLoad[key]:
+            bwLoad[key][keyprime] = BW1[key][keyprime] + BW2[key][keyprime]
+            pass
+        pass
+    return bwLoad
+
+
+def cleanUpBWMatrix(BW, siteConfig):
+    'Clean the matrix by adding the off diagonal terms' 
+    bwLoad = getBwMap(siteConfig)
+    # compute the transpose
+    for key in bwLoad:
+        for keyprime in bwLoad[key]:
+            bwLoad[key][keyprime] = BW[keyprime][key]
+            pass
+        pass
+
+    # sum matrix
+    for key in bwLoad:
+        for keyprime in bwLoad[key]:
+            bwLoad[key][keyprime] =  bwLoad[key][keyprime] + BW[key][keyprime]
+            pass
+        pass
+
+    # divide the diagonal terms by 2 to remove the double counting
+    for key in bwLoad:
+        bwLoad[key][key] =  bwLoad[key][key]/2
+        pass
+
+    # clean the lower off diagonal term
+    i = 0
+    for key in bwLoad:
+        j = 0
+        for keyprime in bwLoad[key]:
+            if(j <= i):
+                j = j + 1
+                continue
+            bwLoad[key][keyprime] = 0
+            j = j + 1            
+            pass
+        i = i + 1
+        pass
+
     return bwLoad
 
 
